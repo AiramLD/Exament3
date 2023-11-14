688ff79ec5d67907340f7d0c37ee53105ae0636a
AiramLD <airamluisdiaz629@gmail.com>
2023-11-14 21:35:59 +0000

    cambio

--- public/list.php
+++ public/list.php
@@ -7,23 +7,23 @@ if (isset($_GET['filter'])) {
   $filter = $_GET['filter'];
 
   if ($filter == 'all') {
-    foreach($shop->showElements() as $e) {
-      echo "<br><p>$e</p>";
+    foreach($shop->showElements() as $i) {
+      echo "<br><p>$i</p>";
     };
   } else if ($filter == 'products') {
-    foreach($shop->showProducts() as $e) {
-      echo "<br><p>$e</p>";
+    foreach($shop->showProducts() as $i) {
+      echo "<br><p>$i</p>";
     };
   } else if ($filter == 'services') {
-    foreach($shop->showElements() as $e) {
-      echo "<br><p>$e</p>";
+    foreach($shop->showElements() as $i) {
+      echo "<br><p>$i</p>";
     };
   } else if ($filter == 'expirationDate') {
-    foreach($shop->showElementsExpirationDate() as $e) {
-      echo "<br><p>$e</p>";
+    foreach($shop->showElementsExpirationDate() as $i) {
+      echo "<br><p>$i</p>";
     };  } else if ($filter == 'unexpired') {
-      foreach($shop->showNoExpired() as $e) {
-        echo "<br><p>$e</p>";
+      foreach($shop->showNoExpired() as $i) {
+        echo "<br><p>$i</p>";
       };
   }
 }
--- src/services.php
+++ src/services.php
@@ -4,6 +4,12 @@ namespace Airam\Examen;
 use Airam\ExamenT3\Elements;
 
 abstract class Services extends Elements {
+    protected $services = [];
+    public function __construct(array $data) {
+        parent::__construct($data['name'], $data['basePrice']);
+        $this->services = $data['services'];
+    }
+    }
     
-}
+
 
